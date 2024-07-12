5c5a1231732c703a358bd6d80a8128ef1dccadaa
SajalDevnath <63283752+SajalDevnath@users.noreply.github.com>
2024-07-12 18:12:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,27 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function mincost(arr) {
+    if (arr.length === 1) return 0; 
+    
+    let minHeap = [];
+    
+    for (let rope of arr) {
+        minHeap.push(rope);
+    }
+    
+    let minCost = 0;
+    
+    while (minHeap.length > 1) {
+        let first = minHeap.shift(); 
+        let second = minHeap.shift(); 
+        
+        let mergeCost = first + second;
+        
+        minCost += mergeCost;
+        
+        minHeap.push(mergeCost);
+        
+    }
+    
+    return minCost;
 }
 
-module.exports=mincost;
+module.exports = mincost;
